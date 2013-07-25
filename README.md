56245c3f6e70ffb447090f564a6bdaf79b031203
fengmk2 <fengmk2@gmail.com>
2013-07-26 02:29:45 +0800

    support repository api

--- README.md
+++ README.md
@@ -61,26 +61,115 @@ Milestone.prototype.list = function (params, callback);
  * Create a milestone.
  * 
  * @param {Object} params
- *  - {Number} id (required) - The ID of a project
- *  - {String} title (required) - The title of an milestone
- *  - {String} [description] (optional) - The description of the milestone
- *  - {String} [due_date] (optional) - The due date of the milestone
+ *  - {Number} id The ID of a project
+ *  - {String} title The title of an milestone
+ *  - {String} [description] The description of the milestone
+ *  - {String} [due_date] The due date of the milestone
  * @param {Function(err, row)} callback
  */
 Milestone.prototype.create = function (params, callback);
 
 /**
  * Update a milestone.
+
  * @param {Object} params
- *  - {Number} id (required) - The ID of a project
- *  - {Number} milestone_id (required) - The ID of a project milestone
- *  - {String} title (required) - The title of an milestone
- *  - {String} [description] (optional) - The description of the milestone
- *  - {String} [due_date] (optional) - The due date of the milestone
- *  - {String} [closed] (optional) - The status of the milestone
+ *  - {Number} id The ID of a project
+ *  - {Number} milestone_id The ID of a project milestone
+ *  - {String} title The title of an milestone
+ *  - {String} [description] The description of the milestone
+ *  - {String} [due_date] The due date of the milestone
+ *  - {String} [closed] The status of the milestone
  * @param {Function(err, row)} callback
  */
 Milestone.prototype.update = function (params, callback);
+```
+
+### Repository
+
+```js
+/**
+ * Get a list of repository branches from a project, sorted by name alphabetically.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ * @param {Function} callback
+ */
+Repository.prototype.getBranches = function (params, callback);
+
+/**
+ * Protects a single project repository branch. 
+ * This is an idempotent function, protecting an already protected repository branch 
+ * still returns a 200 Ok status code.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} branch The name of the branch
+ * @param {Function} callback
+ */
+Repository.prototype.protectBranch = function (params, callback);
+
+/**
+ * Unprotects a single project repository branch. 
+ * This is an idempotent function, unprotecting an already unprotected repository branch 
+ * still returns a 200 Ok status code.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} branch The name of the branch
+ * @param {Function} callback
+ */
+Repository.prototype.unprotectBranch = function (params, callback);
+
+/**
+ * Get a single project repository branch.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} branch The name of the branch
+ * @param {Function} callback
+ */
+Repository.prototype.getBranch = function (params, callback);
+
+/**
+ * Get a list of repository tags from a project, sorted by name in reverse alphabetical order.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ * @param {Function} callback
+ */
+Repository.prototype.getTags = function (params, callback);
+
+/**
+ * Get a list of repository commits in a project.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} [ref_name] The name of a repository branch or tag or if not given the default branch
+ * @param {Function} callback
+ */
+Repository.prototype.getCommits = function (params, callback);
+
+/**
+ * Get a list of repository files and directories in a project.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} [path] The path inside repository, default is '/'. Used to get contend of subdirectories. e.g.: `test`
+ *  - {String} [ref_name] The name of a repository branch or tag or if not given the default branch
+ * @param {Function} callback
+ */
+Repository.prototype.getTree = function (params, callback);
+
+/**
+ * Get the raw file contents for a file.
+ * 
+ * @param {Object} params
+ *  - {String} id The ID of a project
+ *  - {String} sha The commit or branch name
+ *  - {String} filepath The path the file
+ * @param {Function} callback
+ */
+Repository.prototype.getBlob = function (params, callback);
 ```
 
 ## Authors
---
+++ test/repository.test.js
@@ -0,0 +1,224 @@
+/*!
+ * gitlab - test/repository.test.js
+ * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var pedding = require('pedding');
+var client = require('./client');
+
+describe.only('repository.test.js', function () {
+
+  describe('client.repository.list()', function () {
+
+    it('should return a project root / tree', function (done) {
+      client.repository.list({ id: 441, type: 'tree' }, function (err, tree) {
+        should.not.exists(err);
+        should.exists(tree);
+        tree.should.be.instanceof(Array);
+        tree.forEach(function (item) {
+          item.should.have.keys('name', 'type', 'mode', 'id');
+        });
+        done();
+      });
+    });
+
+    it('should return a project /lib tree', function (done) {
+      client.repository.list({ id: 441, type: 'tree', path: 'lib' }, function (err, tree) {
+        should.not.exists(err);
+        should.exists(tree);
+        tree.should.be.instanceof(Array);
+        tree.forEach(function (item) {
+          item.should.have.keys('name', 'type', 'mode', 'id');
+        });
+        done();
+      });
+    });
+
+  });
+
+  describe('client.repository.getTree()', function () {
+
+    it('should return a project root / tree', function (done) {
+      client.repository.getTree({ id: 441 }, function (err, tree) {
+        should.not.exists(err);
+        should.exists(tree);
+        tree.should.be.instanceof(Array);
+        tree.forEach(function (item) {
+          item.should.have.keys('name', 'type', 'mode', 'id');
+        });
+        done();
+      });
+    });
+
+    it('should return a project /lib tree', function (done) {
+      client.repository.getTree({ id: 441, path: 'lib' }, function (err, tree) {
+        should.not.exists(err);
+        should.exists(tree);
+        tree.should.be.instanceof(Array);
+        tree.forEach(function (item) {
+          item.should.have.keys('name', 'type', 'mode', 'id');
+        });
+        done();
+      });
+    });
+
+  });
+
+  describe('client.repository.getBranches()', function () {
+    it('should return a project branches', function (done) {
+      client.repository.getBranches({ id: 441 }, function (err, branches) {
+        should.not.exists(err);
+        should.exists(branches);
+        branches.should.be.instanceof(Array);
+        branches.forEach(function (item) {
+          item.should.have.keys('name', 'commit', 'protected');
+        });
+        done();
+      });
+    });
+  });
+
+  describe('client.repository.getBranch()', function () {
+    it('should return a project master branch', function (done) {
+      client.repository.getBranch({ id: 441, branch: 'master' }, function (err, branch) {
+        should.not.exists(err);
+        should.exists(branch);
+        branch.should.have.keys('name', 'commit', 'protected');
+        branch.name.should.equal('master');
+        done();
+      });
+    });
+
+    it('should return 404 branch not exists', function (done) {
+      client.repository.getBranch({ id: 441, branch: 'master22' }, function (err, branch) {
+        should.exists(err);
+        err.message.should.equal('404 Branch does not exist Not Found');
+        err.statusCode.should.equal(404);
+        should.not.exists(branch);
+        done();
+      });
+    });
+  });
+
+  describe('client.repository.protectBranch() and unprotectBranch()', function () {
+    it('should return protect master branch', function (done) {
+      client.repository.protectBranch({ id: 441, branch: 'master' }, function (err, branch) {
+        should.not.exists(err);
+        should.exists(branch);
+        branch.should.have.keys('name', 'commit', 'protected');
+        branch.name.should.equal('master');
+        branch.protected.should.equal(true);
+        
+        client.repository.protectBranch({ id: 441, branch: 'master' }, function (err, branch) {
+          should.not.exists(err);
+          should.exists(branch);
+          branch.should.have.keys('name', 'commit', 'protected');
+          branch.name.should.equal('master');
+          branch.protected.should.equal(true);
+          done();
+        });
+      });
+    });
+
+    it('should return unprotect master branch', function (done) {
+      client.repository.unprotectBranch({ id: 441, branch: 'master' }, function (err, branch) {
+        should.not.exists(err);
+        should.exists(branch);
+        branch.should.have.keys('name', 'commit', 'protected');
+        branch.name.should.equal('master');
+        branch.protected.should.equal(false);
+        
+        client.repository.unprotectBranch({ id: 441, branch: 'master' }, function (err, branch) {
+          should.not.exists(err);
+          should.exists(branch);
+          branch.should.have.keys('name', 'commit', 'protected');
+          branch.name.should.equal('master');
+          branch.protected.should.equal(false);
+          done();
+        });
+      });
+    });
+
+    it('should return 404 protect branch not exists', function (done) {
+      client.repository.protectBranch({ id: 441, branch: 'master22' }, function (err, branch) {
+        should.exists(err);
+        err.message.should.equal('404 Not Found');
+        err.statusCode.should.equal(404);
+        should.not.exists(branch);
+        done();
+      });
+    });
+
+    it('should return 404 unprotect branch not exists', function (done) {
+      client.repository.unprotectBranch({ id: 441, branch: 'master22' }, function (err, branch) {
+        should.exists(err);
+        err.message.should.equal('404 Not Found');
+        err.statusCode.should.equal(404);
+        should.not.exists(branch);
+        done();
+      });
+    });
+  });
+
+  describe('client.repository.getTags()', function () {
+    it('should return a project tags', function (done) {
+      client.repository.getTags({ id: 441 }, function (err, tags) {
+        should.not.exists(err);
+        should.exists(tags);
+        tags.should.be.instanceof(Array);
+        tags.forEach(function (item) {
+          item.should.have.keys('name', 'commit', 'protected');
+        });
+        done();
+      });
+    });
+  });
+
+  describe('client.repository.getCommits()', function () {
+    it('should return a project commits', function (done) {
+      done = pedding(2, done);
+      client.repository.getCommits({ id: 441 }, function (err, commits) {
+        should.not.exists(err);
+        should.exists(commits);
+        commits.should.be.instanceof(Array);
+        commits.forEach(function (item) {
+          item.should.have.keys('short_id', 'title', 'author_name', 'id', 'author_email', 'created_at');
+        });
+        done();
+      });
+
+      client.repository.getCommits({ id: 441, ref_name: 'master' }, function (err, commits) {
+        should.not.exists(err);
+        should.exists(commits);
+        commits.should.be.instanceof(Array);
+        commits.length.should.above(0);
+        commits.forEach(function (item) {
+          item.should.have.keys('short_id', 'title', 'author_name', 'id', 'author_email', 'created_at');
+        });
+        done();
+      });
+    });
+  });
+
+  describe('client.repository.getBlob()', function () {
+    it('should return a file content', function (done) {
+      client.repository.getBlob({ id: 441, sha: 'master', filepath: 'lib/alidata.js' }, function (err, blob) {
+        should.not.exists(err);
+        should.exists(blob);
+        blob.should.be.instanceof(Buffer);
+        blob.length.should.above(0);
+        blob.toString().should.include('alidata - lib/alidata.js');
+        done();
+      });
+    });
+  });
+
+});
